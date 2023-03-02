57bb8d6326a6ec8340f0ada91478cbc13e215cec
marino74 <marino.aguilera.sola@gmail.com>
2023-03-02 14:44:07 +0100

    local push

--- four-java-impl/src/com/test/Launcher.java
+++ four-java-impl/src/com/test/Launcher.java
@@ -5,7 +5,6 @@ public class Launcher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("xxxxxx local");
-		// System.out.println("xxxxxx repo");
 	}
 
 }
