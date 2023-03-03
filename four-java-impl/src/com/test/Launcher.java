7df1a666c18c5f4e247b6969275cfa0b645b5df5
marino74 <marino.aguilera.sola@gmail.com>
2023-03-03 14:12:17 +0100

    hola999

--- four-java-impl/src/com/test/Launcher.java
+++ four-java-impl/src/com/test/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("xxxxxx intellij-99999");
+		System.out.println("xxxxxx intellij-99999-hola");
 		System.out.println("xxxxxx github-1");
 		System.out.println("xxxxxx eclipse-local");
 	}
