2865ec3f4d710de6efb361197d7ea29a83f59bdc
marino74 <marino.aguilera.sola@gmail.com>
2023-03-03 13:58:43 +0100

    update

--- four-java-impl/src/com/test/Launcher.java
+++ four-java-impl/src/com/test/Launcher.java
@@ -6,11 +6,7 @@ public class Launcher {
 		// TODO Auto-generated method stub
 		System.out.println("xxxxxx intellij-2");
 		System.out.println("xxxxxx github-1");
-<<<<<<< HEAD
 		System.out.println("xxxxxx eclipse");
-=======
-		System.out.println("xxxxxx repo");
->>>>>>> branch 'master' of https://github.com/marino74/four-java.git
 	}
 
 }
