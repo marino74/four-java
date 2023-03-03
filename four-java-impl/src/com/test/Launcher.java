6d344f5f1de0a48009aca65f2a670fbdcbdf62eb
marino74 <marino.aguilera.sola@gmail.com>
2023-03-03 13:49:38 +0100

    Cambio en intellij-2

--- four-java-impl/src/com/test/Launcher.java
+++ four-java-impl/src/com/test/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("xxxxxx intellij-1");
+		System.out.println("xxxxxx intellij-2");
 		//System.out.println("xxxxxx repo");
 	}
 
