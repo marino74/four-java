726ef84f213f251e54f6c0da0c80d704439ea478
marino74 <marino.aguilera.sola@gmail.com>
2023-03-03 14:07:48 +0100

    Cambio en intellij-3

--- four-java-impl/src/com/test/Launcher.java
+++ four-java-impl/src/com/test/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("xxxxxx intellij-2");
+		System.out.println("xxxxxx intellij-3");
 		System.out.println("xxxxxx github-1");
 		System.out.println("xxxxxx eclipse-local");
 	}
